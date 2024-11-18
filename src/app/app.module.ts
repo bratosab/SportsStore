5486ffae8c32414f12ec22651539fe85b8b8d3f1
Abdallah Bratos <bratosab@gmail.com>
2024-11-18 15:50:00 +0100

    feat(update): angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,26 @@ import { StoreFirstGuard } from "./storeFirst.guard";
 @NgModule({
     imports: [BrowserModule, StoreModule,
         RouterModule.forRoot([
-            {
-                path: "store", component: StoreComponent,
-                canActivate: [StoreFirstGuard]
-            },
-            {
-                path: "cart", component: CartDetailComponent,
-                canActivate: [StoreFirstGuard]
-            },
-            {
-                path: "checkout", component: CheckoutComponent,
-                canActivate: [StoreFirstGuard]
-            },
-            {
-                path: "admin",
-                loadChildren: () => import("./admin/admin.module")
-                    .then(m => m.AdminModule),
-                canActivate: [StoreFirstGuard]
-            },
-            { path: "**", redirectTo: "/store" }
-        ])],
+    {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+    },
+    {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+    },
+    {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+    },
+    {
+        path: "admin",
+        loadChildren: () => import("./admin/admin.module")
+            .then(m => m.AdminModule),
+        canActivate: [StoreFirstGuard]
+    },
+    { path: "**", redirectTo: "/store" }
+])],
     providers: [StoreFirstGuard],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
